f44f5d7d0844da7d1280a015ab4f9977afe965f9
WeerasingheMSC <sahanchampathi2001ugc@gmail.com>
2025-11-21 13:23:03 +0530

    install tailwind css for app

---
+++ skillup/app/index.tsx
@@ -0,0 +1,12 @@
+import "../global.css"
+import { View, Text } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text className="bg-red-600 font-bold text-2xl text-center">index fewe</Text>
+    </View>
+  )
+}
+
+export default index
--- skillup/eslint.config.js
+++ skillup/eslint.config.js
@@ -1,4 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require('nativewind/metro');
+ 
+const config = getDefaultConfig(__dirname)
+ 
+module.exports = withNativeWind(config, { input: './global.css' });
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
